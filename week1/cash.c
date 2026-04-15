46100efcfc20d2ad5c5127fb01584b31fdfdcfd3
esraeylullofca <lofca.eylul.esra@ibu.edu.mk>
2026-04-15 18:06:02 +0200

    Refactor cash calculation logic in cash.c

--- week1/cash.c
+++ week1/cash.c
@@ -1,11 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
-// Prompt the user for a non-negative number of cents
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    int cents = get_cents();
+    int coins = 0;
+
+    coins += calculate_quarters(cents);
+    cents = cents % 25;
+
+    coins += calculate_dimes(cents);
+    cents = cents % 10;
+
+    coins += calculate_nickels(cents);
+    cents = cents % 5;
+
+    coins += calculate_pennies(cents);
+
+    printf("%d\n", coins);
+}
+
 int get_cents(void)
 {
     int cents;
-
-    // Re-prompt until a valid non-negative value is entered
     do
     {
         cents = get_int("Change owed: ");
@@ -15,64 +38,21 @@ int get_cents(void)
     return cents;
 }
 
-// Return how many quarters fit in `cents`
 int calculate_quarters(int cents)
 {
     return cents / 25;
 }
 
-// Return how many dimes fit in `cents`
 int calculate_dimes(int cents)
 {
     return cents / 10;
 }
 
-// Return how many nickels fit in `cents`
 int calculate_nickels(int cents)
 {
     return cents / 5;
 }
 
-// Return how many pennies fit in `cents`
-int calculate_pennies(int cents)
-{
-    return cents;
-}
-
-// Prompt the user for a non-negative number of cents
-int get_cents(void)
-{
-    int cents;
-
-    // Re-prompt until a valid non-negative value is entered
-    do
-    {
-        cents = get_int("Change owed: ");
-    }
-    while (cents < 0);
-
-    return cents;
-}
-
-// Return how many quarters fit in `cents`
-int calculate_quarters(int cents)
-{
-    return cents / 25;
-}
-
-// Return how many dimes fit in `cents`
-int calculate_dimes(int cents)
-{
-    return cents / 10;
-}
-
-// Return how many nickels fit in `cents`
-int calculate_nickels(int cents)
-{
-    return cents / 5;
-}
-
-// Return how many pennies fit in `cents`
 int calculate_pennies(int cents)
 {
     return cents;
