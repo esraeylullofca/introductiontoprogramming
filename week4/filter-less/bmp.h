d4bea0f97c81601dca6ef06c8a2d1366dbf15d5f
esraeylullofca <lofca.eylul.esra@ibu.edu.mk>
2026-04-16 15:48:04 +0000

    Thu, Apr 16, 2026, 5:48 PM +02:00

--- week4/filter-less/bmp.h
+++ week4/filter-less/bmp.h
@@ -1,1 +1,138 @@
+#include "helpers.h"
+#include <math.h>
 
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Calculate average of RGB values
+            float average = (image[i][j].rgbtRed +
+                             image[i][j].rgbtGreen +
+                             image[i][j].rgbtBlue) / 3.0;
+
+            int gray = round(average);
+
+            // Assign average to each color channel
+            image[i][j].rgbtRed = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtBlue = gray;
+        }
+    }
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Store original color values
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            // Apply sepia formula
+            int sepiaRed = round(0.393 * originalRed +
+                                 0.769 * originalGreen +
+                                 0.189 * originalBlue);
+
+            int sepiaGreen = round(0.349 * originalRed +
+                                   0.686 * originalGreen +
+                                   0.168 * originalBlue);
+
+            int sepiaBlue = round(0.272 * originalRed +
+                                  0.534 * originalGreen +
+                                  0.131 * originalBlue);
+
+            // Cap values at 255
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            // Assign new values to pixel
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap pixels
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
+// Blur image using box blur
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create a copy of the original image
+    RGBTRIPLE copy[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Apply blur filter
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumRed = 0;
+            int sumGreen = 0;
+            int sumBlue = 0;
+            int count = 0;
+
+            // Loop through neighboring pixels (3x3 grid)
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int newRow = i + di;
+                    int newCol = j + dj;
+
+                    // Check boundaries
+                    if (newRow >= 0 && newRow < height &&
+                        newCol >= 0 && newCol < width)
+                    {
+                        sumRed += copy[newRow][newCol].rgbtRed;
+                        sumGreen += copy[newRow][newCol].rgbtGreen;
+                        sumBlue += copy[newRow][newCol].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            // Calculate average and assign blurred values
+            image[i][j].rgbtRed = round((float) sumRed / count);
+            image[i][j].rgbtGreen = round((float) sumGreen / count);
+            image[i][j].rgbtBlue = round((float) sumBlue / count);
+        }
+    }
+}
