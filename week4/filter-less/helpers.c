28b7022a7bcc987ab0ca7e6ddeb53728a8bc0c0e
esraeylullofca <lofca.eylul.esra@ibu.edu.mk>
2026-04-17 22:53:24 +0000

    Sat, Apr 18, 2026, 12:53 AM +02:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -1,7 +1,7 @@
 #include "helpers.h"
 #include <math.h>
 
-// grayscale
+// GRAYSCALE
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
@@ -19,7 +19,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     }
 }
 
-// reflect
+// REFLECT
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
@@ -33,7 +33,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     }
 }
 
-// blur
+// BLUR
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy[height][width];
@@ -72,78 +72,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = round((float) sumR / count);
             image[i][j].rgbtGreen = round((float) sumG / count);
             image[i][j].rgbtBlue = round((float) sumB / count);
-        }
-    }
-}
-
-// edges
-void edges(int height, int width, RGBTRIPLE image[height][width])
-{
-    RGBTRIPLE copy[height][width];
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            copy[i][j] = image[i][j];
-        }
-    }
-
-    int Gx[3][3] = {
-        {-1, 0, 1},
-        {-2, 0, 2},
-        {-1, 0, 1}
-    };
-
-    int Gy[3][3] = {
-        {-1, -2, -1},
-        {0, 0, 0},
-        {1, 2, 1}
-    };
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            int Rx = 0, Ry = 0;
-            int GxSum = 0, GySum = 0;
-            int Bx = 0, By = 0;
-
-            for (int di = -1; di <= 1; di++)
-            {
-                for (int dj = -1; dj <= 1; dj++)
-                {
-                    int ni = i + di;
-                    int nj = j + dj;
-
-                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
-                    {
-                        int kx = Gx[di + 1][dj + 1];
-                        int ky = Gy[di + 1][dj + 1];
-
-                        Rx += copy[ni][nj].rgbtRed * kx;
-                        Ry += copy[ni][nj].rgbtRed * ky;
-
-                        GxSum += copy[ni][nj].rgbtGreen * kx;
-                        GySum += copy[ni][nj].rgbtGreen * ky;
-
-                        Bx += copy[ni][nj].rgbtBlue * kx;
-                        By += copy[ni][nj].rgbtBlue * ky;
-                    }
-                }
-            }
-
-            int red = round(sqrt(Rx * Rx + Ry * Ry));
-            int green = round(sqrt(GxSum * GxSum + GySum * GySum));
-            int blue = round(sqrt(Bx * Bx + By * By));
-
-            if (red > 255) red = 255;
-            if (green > 255) green = 255;
-            if (blue > 255) blue = 255;
-
-            image[i][j].rgbtRed = red;
-            image[i][j].rgbtGreen = green;
-            image[i][j].rgbtBlue = blue;
         }
     }
 }
--- week4/filter-less/helpers.h
+++ week4/filter-less/helpers.h
@@ -1,7 +1,10 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
 #include "bmp.h"
 
-// Function prototypes
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 void blur(int height, int width, RGBTRIPLE image[height][width]);
-void edges(int height, int width, RGBTRIPLE image[height][width]);
+
+#endif
