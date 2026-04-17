e16e6caf1042e657f8bda9c37d19d49f7cf36f8c
esraeylullofca <lofca.eylul.esra@ibu.edu.mk>
2026-04-17 22:51:47 +0000

    Sat, Apr 18, 2026, 12:51 AM +02:00

--- week4/filter-less/bmp.h
+++ week4/filter-less/bmp.h
@@ -3,47 +3,40 @@
 
 #include <stdint.h>
 
-// Aliases
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
 typedef uint16_t WORD;
 
-// File header
 typedef struct
 {
-    WORD   bfType;
-    DWORD  bfSize;
-    WORD   bfReserved1;
-    WORD   bfReserved2;
-    DWORD  bfOffBits;
-} __attribute__((__packed__))
-BITMAPFILEHEADER;
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
+} __attribute__((__packed__)) BITMAPFILEHEADER;
 
-// Info header
 typedef struct
 {
-    DWORD  biSize;
-    LONG   biWidth;
-    LONG   biHeight;
-    WORD   biPlanes;
-    WORD   biBitCount;
-    DWORD  biCompression;
-    DWORD  biSizeImage;
-    LONG   biXPelsPerMeter;
-    LONG   biYPelsPerMeter;
-    DWORD  biClrUsed;
-    DWORD  biClrImportant;
-} __attribute__((__packed__))
-BITMAPINFOHEADER;
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
+} __attribute__((__packed__)) BITMAPINFOHEADER;
 
-// Pixel
 typedef struct
 {
-    BYTE  rgbtBlue;
-    BYTE  rgbtGreen;
-    BYTE  rgbtRed;
-} __attribute__((__packed__))
-RGBTRIPLE;
+    BYTE rgbtBlue;
+    BYTE rgbtGreen;
+    BYTE rgbtRed;
+} __attribute__((__packed__)) RGBTRIPLE;
 
 #endif
