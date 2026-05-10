15f432ef635924779a77543f26a6e656d07b7fc2
esraeylullofca <lofca.eylul.esra@ibu.edu.mk>
2026-05-10 07:16:21 +0200

    Complete hello0

--- week10/hello0/app.py
+++ week10/hello0/app.py
@@ -1,9 +1,6 @@
-# Says hello to world by returning a string of text
-
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
--- week10/hello4/app.py
+++ week10/hello4/app.py
@@ -1,14 +1,8 @@
-# Uses parameter with same name as variable
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-        name = "world"
+    name = request.args.get("name", "world")
     return render_template("index.html", name=name)
